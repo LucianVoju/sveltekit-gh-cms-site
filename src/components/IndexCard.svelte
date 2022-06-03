bde5bf8a9b7a3a51932c6e0c92e4574c013db7cc
Voju Lucian <voju.lucian@gmail.com>
2022-06-03 10:54:58 +0300

    restyle

--- src/components/IndexCard.svelte
+++ src/components/IndexCard.svelte
@@ -1,35 +1,65 @@
 <script>
-	// href={item.slug} title={item.data.title} date={item.data.date}
-	export let href = '#';
+	// {
+	//   type: 'blog',
+	//   content: '',
+	//   frontmatter: {
+	//     title: 'This is a test post using Github Issues as CMS',
+	//     description: 'Test description'
+	//   },
+	//   title: 'This is a test post using Github Issues as CMS',
+	//   description: 'Test description',
+	//   tags: [],
+	//   slug: 'this-is-a-test-post-using-github-issues-as-cms',
+	//   date: '2022-05-22T09:18:13.000Z',
+	//   ghMetadata: {
+	//     issueUrl: 'https://github.com/LucianVoju/vojulucian.com/issues/1',
+	//     commentsUrl: 'https://api.github.com/repos/LucianVoju/vojulucian.com/issues/1/comments',
+	//     title: 'welcome',
+	//     created_at: '2022-05-22T09:18:13Z',
+	//     updated_at: '2022-05-31T07:18:12Z',
+	//     reactions: {
+	//       url: 'https://api.github.com/repos/LucianVoju/vojulucian.com/issues/1/reactions',
+	//       total_count: 0,
+	//       '+1': 0,
+	//       '-1': 0,
+	//       laugh: 0,
+	//       hooray: 0,
+	//       confused: 0,
+	//       heart: 0,
+	//       rocket: 0,
+	//       eyes: 0
+	//     }
+	//   }
+	// }
 
-	/** @type {import('$lib/types').GHMetadata} */
-	export let ghMetadata = null;
-	export let title = 'Untitled post';
-	/** @type {string} */
-	export let stringData = 'no date';
+	export let item;
+	let date = new Date(item?.date).toISOString().slice(0, 10);
 </script>
 
-<a
-	sveltekit:prefetch
-	class="w-full text-gray-900 hover:text-lime-600 dark:text-gray-100 dark:hover:text-lime-100"
-	{href}
-	><div class="mb-8 w-full">
-		<div class="flex flex-col justify-between md:flex-row">
-			<h4 class="mb-2 w-full flex-auto text-lg font-medium md:text-xl">
-				{title}
-			</h4>
-			<div class="inline-flex flex-1 items-center">
-				{#if ghMetadata && ghMetadata.reactions.total_count}
-					<span
-						class=" mr-2 min-w-[2rem] font-mono text-xs text-gray-700 text-opacity-70 dark:text-gray-300"
-						>{ghMetadata.reactions.total_count} ♥</span
-					>
-				{/if}
-				<p class="mb-4 w-32 text-left text-gray-500 md:mb-0 md:text-right">{stringData}</p>
-			</div>
+<div class="mb-4 w-full">
+	<div class="flex flex-col-reverse justify-between md:flex-row">
+		<a
+			sveltekit:prefetch
+			class="w-full text-gray-900 hover:text-lime-600 dark:text-gray-100 dark:hover:text-lime-100"
+			href={`/` + item.slug}
+			><h4 class="mb-2 w-full flex-auto text-lg font-medium md:text-xl">
+				<!-- <span class="rounded bg-blue-700 p-1 text-xs text-white">Essay</span> -->
+				<span>{item.title}</span>
+			</h4></a
+		>
+		<div
+			class="inline-flex flex-1 flex-row-reverse items-center justify-end md:flex-row md:justify-start"
+		>
+			{#if (item.ghMetadata && item.ghMetadata.reactions.total_count) || item.data?.devToReactions}
+				<span
+					class=" mr-2 min-w-[3rem] text-right font-mono text-xs text-gray-700 text-opacity-70 dark:text-gray-300"
+					>{(item?.ghMetadata?.reactions?.total_count || 0) + (item?.data?.devToReactions || 0)} ♥</span
+				>
+			{/if}
+			<p class="w-28 text-left text-gray-500 md:text-right">{date}</p>
 		</div>
-		<p class="text-gray-600 dark:text-gray-400">
-			<slot />
-		</p>
-	</div></a
->
+	</div>
+	<p class="overflow-hidden text-ellipsis break-all text-sm text-gray-600 dark:text-gray-400">
+		{item.description}
+	</p>
+</div>
