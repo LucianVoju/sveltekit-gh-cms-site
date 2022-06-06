84a369ca5909d87da946edaf99f366866e582584
Voju Lucian <voju.lucian@gmail.com>
2022-06-06 13:34:27 +0300

    update seite config

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -6,10 +6,9 @@ export const GH_USER = 'https://github.com/LucianVoju';
 export const SITE_TITLE = "Lucian's site";
 export const SITE_DESCRIPTION =
 	"Voju Lucian's officila site. Full Stack JavaScript Developer, Weightlifter, Continuous Learner";
-export const DEFAULT_OG_IMAGE =
-	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
+export const DEFAULT_OG_IMAGE = '';
 export const MY_TWITTER_HANDLE = 'vojulucian';
-export const MY_YOUTUBE = 'https://youtube.com';
+// export const MY_YOUTUBE = 'https://youtube.com';
 
 // dont forget process.env.GH_TOKEN
 // if supplied, raises rate limit from 60 to 5000
